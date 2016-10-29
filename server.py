40543573a1bf2945f5ad4005df3e6e6c952ee78f
bstadt <brandonduderstadt@gmail.com>
2016-10-29 16:28:04 -0400

    added skeleton of front page for case study at tomorrows meeting

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
-#rhis is a change
-
 from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
+
+def getAnnouncements():
+    #will eventually read announcements from data base
+    return [
+            ['Test Header', 'This is a test body'],
+            ['Test Header', 'This is a test body'],
+            ['Test Header', 'This is a test body']
+        ]
 
 @app.route('/', methods = ['GET'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template('index.html', announcements = getAnnouncements())
 
 if __name__ == '__main__':
     app.run(debug = True)
