ca6bf8ba06654f072f7b13161e772291ef4ff488
bstadt <brandonduderstadt@gmail.com>
2016-11-30 19:39:19 -0500

    got naive announcements working

--- server.py
+++ server.py
@@ -1,18 +1,27 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
+announcementsLog = []
+
 def getAnnouncements():
-    #will eventually read announcements from data base
-    return [
-            ['Test Header', 'This is a test body'],
-            ['Test Header', 'This is a test body'],
-            ['Test Header', 'This is a test body']
-        ]
+    return announcementsLog
 
 @app.route('/', methods = ['GET'])
 def index():
     if request.method == 'GET':
         return render_template('index.html', announcements = getAnnouncements())
+
+@app.route('/submitAnnouncement', methods = ['GET', 'POST'])
+def submitAnnouncement():
+    if request.method == 'GET':
+        return render_template('submitAnnouncement.html')
+    if request.method == 'POST':
+        header = str(request.form['Header'])
+        body = str(request.form['Body'])
+        announcementsLog.append([header, body])
+        return redirect(url_for('index'))
+    return 404
+
 
 if __name__ == '__main__':
     app.run(debug = True)
